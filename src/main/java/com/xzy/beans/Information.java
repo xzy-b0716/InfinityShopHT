ae7983c0a6887ffc3627a2c66d864e74a8cfda2d
ssz996 <zss_super@163.com>
2019-08-13 00:48:50 +0800

    update second

---
+++ src/main/java/com/xzy/beans/Information.java
@@ -0,0 +1,56 @@
+package com.xzy.beans;
+
+/**
+ * Create by Shuai on 2019/8/12 18:04
+ */
+public class Information {
+    private ProductBrand productBrand;
+
+    private ProductMaterial productMaterial;
+
+    private ProductVersion productVersion;
+
+    private ProductStyle productStyle;
+
+    private ProductThickness productThickness;
+
+    public ProductBrand getProductBrand() {
+        return productBrand;
+    }
+
+    public void setProductBrand(ProductBrand productBrand) {
+        this.productBrand = productBrand;
+    }
+
+    public ProductMaterial getProductMaterial() {
+        return productMaterial;
+    }
+
+    public void setProductMaterial(ProductMaterial productMaterial) {
+        this.productMaterial = productMaterial;
+    }
+
+    public ProductVersion getProductVersion() {
+        return productVersion;
+    }
+
+    public void setProductVersion(ProductVersion productVersion) {
+        this.productVersion = productVersion;
+    }
+
+    public ProductStyle getProductStyle() {
+        return productStyle;
+    }
+
+    public void setProductStyle(ProductStyle productStyle) {
+        this.productStyle = productStyle;
+    }
+
+    public ProductThickness getProductThickness() {
+        return productThickness;
+    }
+
+    public void setProductThickness(ProductThickness productThickness) {
+        this.productThickness = productThickness;
+    }
+}
---
+++ src/main/java/com/xzy/beans/ProductMaterial.java
@@ -0,0 +1,26 @@
+package com.xzy.beans;
+
+/**
+ * Create by Shuai on 2019/8/12 21:19
+ */
+public class ProductMaterial {
+    private String name="质地";
+    private String describe;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+}
